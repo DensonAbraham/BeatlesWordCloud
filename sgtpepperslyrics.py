6dfe9d9284839c10ea1724df1f2cf061edbf8853
Nick Browning <56777652+softver787@users.noreply.github.com>
2020-08-27 10:48:27 +1000

    Add files via upload

---
+++ sgtpepperslyrics.py
@@ -0,0 +1,42 @@
+from wordcloud import WordCloud, STOPWORDS # creates wordcloud
+import matplotlib.pyplot as plt # displays wordcloud
+import pandas as pd # reads csv
+import numpy as np # to get the color of our image
+from PIL import Image # loads image
+
+# reads the comma seperated data
+df = pd.read_csv(r"sgtpeppers.csv", encoding ="latin-1")
+
+# initialize the text
+lyric_words = ""
+
+# eliminates certain words
+stopwords = set(STOPWORDS)
+
+for val in df.songContent:
+    # converts each val to a string
+    val = str(val) 
+  
+    # split the value
+    tokens = val.split() 
+      
+    # converts each token into lowercase 
+    for i in range(len(tokens)): 
+        tokens[i] = tokens[i].lower() 
+      
+    lyric_words += " ".join(tokens)+" "
+
+# gets color of the image
+custom_image = np.array(Image.open("cloud.png"))
+
+# creates wordcloud
+wordcloud = WordCloud(background_color ='white', 
+                stopwords = stopwords, 
+                mask = custom_image).generate(lyric_words)
+
+
+# displays the wordcloud
+plt.imshow(wordcloud, interpolation = 'bilinear') 
+plt.axis("off") 
+plt.tight_layout(pad = 0)   
+plt.show() 
